6d5263bfc0b280887431ce7537a9c01dd5c75a9d
iharis <charalampos.ioannou@gmail.com>
2017-12-07 23:03:10 +0200

    initial commit for the UI

---
+++ Software Eng. UI/js/activity_edit-controller.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    //Handle File Upload
+    $(".btn-add").click(function(){
+        e.preventDefault();
+
+        var controlForm = $('.controls:first'),
+            currentEntry = $(this).parents('.entry:first'),
+            newEntry = $(currentEntry.clone()).appendTo(controlForm);
+
+        newEntry.find('input').val('');
+        controlForm.find('.entry:not(:last) .btn-add')
+            .removeClass('btn-add').addClass('btn-remove')
+            .removeClass('btn-success').addClass('btn-danger')
+            .html('<span class="glyphicon glyphicon-minus"></span>');
+    });
+    $(".btn-remove").click(function(){
+        $(this).parents('.entry:first').remove();
+        e.preventDefault();
+        return false;
+    });
+
+    //Handle Address Type
+    $("#activity-form-address");
+    $("#activity-form-map");
+
+    //Handle Form Submition
+    $("#activity-form-submit").click(function(){
+        var result_data = {
+            "title": $("#activity-form-title").val(),
+            "description": $("#activity-form-description").val(),
+            "ticket_number": $("#activity-form-ticket_number").val(),
+            "ticket_price": $("#activity-form-ticket_price").val(),
+            "date": $("#activity-form-date").val(),
+            "address": $("#activity-form-address").val()
+        };
+        console.log("Data: "+JSON.stringify(result_data, null, 2));
+        
+    });
+
+});
